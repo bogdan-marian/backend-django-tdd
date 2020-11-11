43250d01f10dac96d128d02e4905cb59fc026ef1
Bogdan <bogdan.oloeriu@gmail.com>
2020-11-11 11:00:07 +0100

    check user has an email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -22,3 +22,9 @@ class ModelTests(TestCase):
         
         self.assertEqual(user.email, email.lower())
 
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raise error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'password123')
+            
+
