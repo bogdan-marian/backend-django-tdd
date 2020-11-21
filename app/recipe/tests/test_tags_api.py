6d3e6d6206b7a6e67d5052f6681cb08805c98dc0
Bogdan <bogdan.oloeriu@gmail.com>
2020-11-21 11:47:11 +0100

    54 add feature list tags

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -9,6 +9,7 @@ from core.models import Tag
 
 from recipe.serializers import TagSerializer
 
+
 TAGS_URL = reverse('recipe:tag-list')
 
 
@@ -19,38 +20,39 @@ class PublicTagsApiTests(TestCase):
         self.client = APIClient()
 
     def test_login_required(self):
-        """Test that login is required for retriving tags"""
+        """Test that login required for retrieving tags"""
         res = self.client.get(TAGS_URL)
+
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class PrivateTagsApiTests(TestCase):
-    """Test the authorizes user tags API"""
+    """Test the authorized user tags API"""
 
     def setUp(self):
         self.user = get_user_model().objects.create_user(
-            'test@chessout.eu',
-            'password123'
+            'test@londonappdev.com',
+            'password'
         )
         self.client = APIClient()
         self.client.force_authenticate(self.user)
 
-    def test_retrive_tags(self):
-        """Test retriving tags"""
+    def test_retrieve_tags(self):
+        """Test retrieving tags"""
         Tag.objects.create(user=self.user, name='Vegan')
         Tag.objects.create(user=self.user, name='Dessert')
 
         res = self.client.get(TAGS_URL)
+
         tags = Tag.objects.all().order_by('-name')
         serializer = TagSerializer(tags, many=True)
-
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
 
     def test_tags_limited_to_user(self):
-        """Test that tags returned are for the authenticated user"""
+        """Test that tags returned are for authenticated user"""
         user2 = get_user_model().objects.create_user(
-            'other@chessout.eu'
+            'other@londonappdev.com',
             'testpass'
         )
         Tag.objects.create(user=user2, name='Fruity')
